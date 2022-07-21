4e33f7ff538a3f608b1fa6f9343090edec86e732
azouiaymen <azouiaimen97@gmail.com>
2022-07-21 21:37:08 +0200

    qdding sketch bqsics

--- main.py
+++ main.py
@@ -18,16 +18,27 @@ drop_down_box = html.Div(
            'text-align': 'center'}
 )
 
-# section for the graphs 
+# section for the graphs
 Graphs = html.Div(
     children=[
-        html.Div(children=['Graph_1',dcc.Graph()]),
-        html.Div(children=['Graph_2',dcc.Graph()])
+        html.Div(children=['Graph_1', dcc.Graph()],
+                 style={'width': '45%', "position": "fixed", "left": "2%",
+                 'display': 'inline-block', 'text-align': 'center',
+                        'top': '10%', 'border': '2px solid red', 'height': '50%'}),
+        html.Div(children=['Graph_2', dcc.Graph()],
+                 style={'width': '45%', 'position': 'fixed', 'left': '53%',
+                 'display': 'inline-block', 'text-align': 'center',
+                        'top': '10%', 'border': '3px solid red', 'height': '50%'})
     ]
 )
 
-app.layout = html.Div()
+app.layout = html.Div(id='main_div', children=[drop_down_box, Graphs],
+                      style={'background-color': '#FFFFFF', 'padding': '0',
+                             'width': '100%', 'height': '100%', 'position': 'fixed',
+                             'top': '0%', 'left': '0%', 'bottom': '0%', 'height': '100%',
+                             'border': '2px solid blue'}
+                      )
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8080)
+    app.run_server(debug=False, port=8080)
